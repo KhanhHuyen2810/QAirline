bf566e2fc8dc36b121e53fcd6c5233b562f5d241
KhanhHuyen2810 <145785839+KhanhHuyen2810@users.noreply.github.com>
2024-12-06 03:23:00 +0700

    Complete Homepage

--- obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b04764cc412ebac6931559fa2525ea1cf5ce0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e04286a55eef1e9fef9cad79dce19b9f200974")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
